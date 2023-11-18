43d4c30247c8858f746da6cd1e9fc5c67f22423b
Atsilfia Alfath Syam <atsilfiaalfath@gmail.com>
2023-11-18 23:56:03 +0700

    update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,11 +42,18 @@ def create_casual_rent_df(d_df):
     }).reset_index()
     return daily_casual_rent_df
 
+def create_monthly_rent_df(d_df):
+    monthly_rent_df = d_df.groupby(by=["year", "month"]).agg({
+        'total_count_user': 'sum'
+    }).reset_index()
+    return monthly_rent_df
+   
 
 # monthly
 def create_season_rent_df(d_df):
     season_rent_df = d_df.groupby(by='season')[['registered', 'casual']].sum().reset_index()
     return season_rent_df
+
 
 min_date =rent_day_df["dateday"].min()
 max_date = rent_day_df["dateday"].max()
@@ -74,6 +81,7 @@ daily_rent_df = create_daily_rent_df(day_main_df)
 daily_registered_rent_df = create_registered_rent_df(day_main_df)
 daily_casual_rent_df = create_casual_rent_df(day_main_df)
 monthly_rent_df = create_monthly_rent_df(day_main_df)
+season_rent_df = create_season_rent_df(day_main_df)
 
 
 st.header('Ats BR Dashboard 🚲')
@@ -96,9 +104,97 @@ with col3:
     st.metric('Total User', value= daily_rent_total)
 
 
+# plot total perfilter day
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_rent_df["dateday"],
+    daily_rent_df["total_count_user"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ax.set_title("Grafik user", loc="center", fontsize=30)
+ax.set_xlabel("Rentang waktu")
+ax.set_ylabel("Jumlah user")
+st.pyplot(fig)
 
 
+# plot monthly
+fig, ax = plt.subplots(figsize=(16, 12))
+months_order = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+]
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    x="month", 
+    y="total_count_user",
+    data= monthly_rent_df.sort_values(by="total_count_user", ascending=False),
+    palette=colors,
+    ax=ax,
+    order=months_order
+)
+ax.set_title("Monthly Rentals", loc="center", fontsize=30)
+ax.set_ylabel("Month")
+ax.set_xlabel("Total user")
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
 
 
+st.subheader('Per season')
+col1, col2= st.columns(2)
+ 
+with col1:
+    st.subheader('Register user ')
+    # plot season regiestered user
+    fig, ax = plt.subplots(figsize=(16, 12))
+
+    colors = ["#90CAF9", "#D3D3D3", "#D3D3D3"]
+    sorted_r_df = season_rent_df.sort_values(by="registered", ascending=False)
+    sns.barplot(
+        x="season", 
+        y="registered",
+        data= sorted_r_df,
+        palette=colors,
+        ax=ax,
+    )
+    for index, row in enumerate(sorted_r_df['registered']):
+        ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+  
+    ax.set_title(" Register user Rentals in Season", loc="center", fontsize=30)
+    ax.set_ylabel("Month")
+    ax.set_xlabel("Total Registered user")
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=15)
+    st.pyplot(fig)
+
+with col2:
+    st.subheader('Casual user ')
+
+    # plot season registered user
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    colors = ["#90CAF9", "#D3D3D3", "#D3D3D3"]
+    sorted_df = season_rent_df.sort_values(by="casual", ascending=False)
+    sns.barplot(
+        x="season", 
+        y="casual",
+        data=sorted_df,
+        palette=colors,
+        ax=ax,
+    )
+    for index, row in enumerate(sorted_df['casual']):
+        ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+  
+    ax.set_title("Casual user rental in season", loc="center", fontsize=30)
+    ax.set_ylabel("Total casual user")
+    ax.set_xlabel("Month")
+    ax.tick_params(axis='y', labelsize=20)
+    ax.tick_params(axis='x', labelsize=15)
+    st.pyplot(fig)
+
+    
 
 
