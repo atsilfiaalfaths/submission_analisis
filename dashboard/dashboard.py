2a16e164cada1701ca72d5b83b203dfcf303127c
Atsilfia Alfath Syam <atsilfiaalfath@gmail.com>
2023-11-19 14:51:47 +0700

    update requierement

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from PIL import Image
-from babel.numbers import format_currency
 sns.set(style='dark')
 
 
